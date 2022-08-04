321d2dd1daa610bd15b3bbb21ba7455cfa3bb2ea
NoamZeise <noam4134@gmail.com>
2022-08-05 00:20:01 +0100

    c++ indent fix

--- init.el
+++ init.el
@@ -155,3 +155,9 @@
 
 ;;stop ding
 (setq visible-bell 1)
+
+;;c++ fix indent
+(defun my-c++-mode-hook ()
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
