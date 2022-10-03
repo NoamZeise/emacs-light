76d0c947a4dbb4fe2cac0754bc22bd0f150aaddf
NoamZeise <noam4134@gmail.com>
2022-10-03 12:48:26 +0100

    cleaned up init, removed unused code, better comments

--- init.el
+++ init.el
@@ -2,15 +2,16 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; uncomment for first install if get key signature error
-(setq package-check-signature nil)
+;; uncomment for first install if get key signature error or not connecting to server
+;; (setq package-check-signature nil)
 
+;; stop emacs from making backups in same dir as file being edited
 (setq backup-directory-alist '(("." . "~/.emacs.d/.backups/")))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
-
+;; list of packages to install
 (setq package-selected-packages '(
 				  lsp-mode
 				  yasnippet
@@ -32,44 +33,41 @@
 				  lsp-java
 				  )
       )
-
+;; install each package in above list
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
   (mapc #'package-install package-selected-packages))
 
-;; completion everywhere
+;; completion suggestion everywhere
 (global-company-mode)
 
-;; helm setup
+;;; helm setup
 (helm-mode)
 (require 'helm-xref)
 (define-key global-map [remap find-file] #'helm-find-files)
 (define-key global-map [remap execute-extended-command] #'helm-M-x)
 (define-key global-map [remap switch-to-buffer] #'helm-mini)
 
-;; common lisp setup
+;;; common lisp setup
 (setq inferior-lisp-program "sbcl")
-;; open dly automatically on lisp file
+;; open sly automatically on lisp file
 (add-hook 'sly-mode-hook
           (lambda ()
             (unless (sly-connected-p)
               (save-excursion (sly)))))
 
-;; java config
+;;; java setup
 (require 'lsp-java)
-;;(setq lsp-java-vmargs (list
-;;                       "-noverify"
-;;                       "--enable-preview"))
-;;(add-hook 'java-mode-hook #'lsp)
 (require 'dap-java)
 (dap-auto-configure-mode)
 (require 'lsp-java-boot)
 ;; to enable the lenses
 (add-hook 'lsp-mode-hook #'lsp-lens-mode)
 (add-hook 'java-mode-hook #'lsp-java-boot-lens-mode)
+;; disable java-lsp formatting
 (setq lsp-java-format-on-type-enabled nil)
 
-;; lsp setup
+;;; lsp setup
 (which-key-mode)
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
@@ -82,12 +80,7 @@
   (c-set-offset 'substatement-open 0))
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
-(defun my-java-mode-hook()
-  (setq c-electric-flag nil))
-
-(add-hook 'java-mode-hook 'my-java-mode-hook)
-
-;; rust setup 
+;;; rust setup 
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
 
@@ -104,33 +97,33 @@
   (require 'dap-cpptools)
   (yas-global-mode))
 
-;; remove emacs menus
+;;; remove emacs menus
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; all the icons
+;;; all the icons
 (add-to-list 'load-path "~/.emacs.d/all-the-icons.el/")
 (require 'all-the-icons)
 
-;; treemacs config
+;;; treemacs config
 (setq treemacs-width 20)
 (setq treemacs-text-scale -1.2)
 (treemacs-resize-icons 22)
 (require 'treemacs-all-the-icons)
 (treemacs-load-theme "all-the-icons")
-;;(add-hook 'emacs-startup-hook 'treemacs)
+;;(add-hook 'emacs-startup-hook 'treemacs) ; for starting up treemacs automatically
 
-;; centaur tabs
-(setq centaur-tabs-enable-key-bindings t) ;;require before load
+;;; centaur tabs
+(setq centaur-tabs-enable-key-bindings t) ; require before load
 (require 'centaur-tabs)
 ;;(centaur-tabs-mode t)
 (setq centaur-tabs-set-icons t)
 (centaur-tabs-headline-match)
 
-;; nano theme setup
+;;; nano theme setup
 (add-to-list 'load-path "~/.emacs.d/nano-emacs")
 (require 'nano-theme-dark)
 (require 'nano-faces) (nano-faces)
@@ -142,30 +135,23 @@
 ;; show git changed/added/removed lines at side
 ;;(global-git-gutter-mode +1)
 
-;; show numbers on side of file
+;; show numbers on side of programming files
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; remove menus
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; fix helm at bottom
+;; fix helm to bottom of frame
 (add-to-list 'display-buffer-alist
                     `(,(rx bos "*helm" (* not-newline) "*" eos)
                          (display-buffer-in-side-window)
                          (inhibit-same-window . t)
                          (window-height . 0.4)))
 
-;; fix shell open on side
+;; fix shell open on same window as cursor
 (add-to-list 'display-buffer-alist
              `(,(rx bos "*shell*")
                display-buffer-same-window
                (reusable-frames . visible)))
 
-;; have buffers open on current window
+;; have buffers open on same window as cursor
 (defun my-list-buffers (&optional arg)
   (interactive "P")
   (switch-to-buffer (list-buffers-noselect arg)))
